457a7b7822a061109a07531e64e22e3f9573568b
mangnei2 <mangnei58@gmail.com>
2022-06-01 21:03:34 +0800

    handleData section

---
+++ main.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('.input_text');
+const main = document.querySelector('#name');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.desc');
+const clouds = document.querySelector('.clouds');
+const button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374&units=metric')
+.then(response => response.json())
+
+// handleData
+.then(data => {
+  const tempValue = data['main']['temp'];
+  const nameValue = data['name'];
+  const descValue = data['weather'][0]['description'];
+
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})})
